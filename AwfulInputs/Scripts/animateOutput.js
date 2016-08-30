483aab7cfbcfe564fb193f0d9af44c76d799b469
michael-dean-haynie <michael.dean.haynie@gmail.com>
2016-08-29 21:42:43 -0700

    Added output fadeIn and fadeOut

---
+++ AwfulInputs/Scripts/animateOutput.js
@@ -0,0 +1,19 @@
+﻿function animateOutput(id){
+    var output = $("#" + id);
+    if (output.attr("data-is-visible") == 'true') {
+        output.fadeOut(500, function () {
+            output.empty();
+            output.append(output.attr("data-output"));
+            output.fadeIn(500, function () {
+                output.attr("data-is-visible", "true");
+            });
+        });
+    } else if (output.attr("data-is-visible") == 'false') {
+        output.fadeOut(0);
+        output.empty();
+        output.append(output.attr("data-output"));
+        output.fadeIn(500, function () {
+            output.attr("data-is-visible", "true");
+        });
+    }
+}
