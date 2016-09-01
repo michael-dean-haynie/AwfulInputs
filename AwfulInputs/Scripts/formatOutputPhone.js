0148f13255a6dd86e1b1a31c6a97df1b778d3a98
michael-dean-haynie <michael.dean.haynie@gmail.com>
2016-08-31 23:14:14 -0700

    Added phone formating and output styles

---
+++ AwfulInputs/Scripts/formatOutputPhone.js
@@ -0,0 +1,26 @@
+﻿function formatOutputPhone(outputID) {
+    var element = $("#" + outputID);
+    var input = element.attr("data-output");
+    var errMsg = "";
+
+    // Format output
+    var input = input.substr(0, 20);
+    var allNums = ""
+    for (var i = 0; i < input.length; i++) {
+        allNums += "0123456789".indexOf(input[i]) == -1 ? "" : input[i];
+    }
+    output = "(" + allNums.substr(0, 3) + ") " + allNums.substr(3, 3) + "-" + allNums.substr(6);
+
+    // check for and set errors
+    if (allNums.length != 10) errMsg = "Please enter a 10 digit phone number";
+
+    // Replace "data-output" attribute with formatted output or error message
+    if (errMsg != "") {
+        $("#" + outputID).attr("data-output", errMsg);
+        element.attr("data-format-status", "success");
+    } else {
+        $("#" + outputID).attr("data-output", output);
+        element.attr("data-format-status", "error");
+
+    }
+}
